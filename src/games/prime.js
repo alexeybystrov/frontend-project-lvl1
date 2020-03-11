391b1d5ed862c8a4f3deb42fbbbc59bdee4d2d96
Alexey Bystrov <alexeybystrov@me.com>
2020-03-12 00:21:58 +0300

    refactor prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,21 +3,21 @@ import { intro, randomizer, gameEngine } from '../index.js';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (num) => {
+  for (let divisor = 2; divisor < num; divisor += 1) {
+    if (num % divisor === 0) {
+      return false;
+    }
+  } return true;
+};
+
 const generateQuestionAndAnswer = () => {
   const min = 2;
   const max = 20;
 
   const question = randomizer(min, max);
 
-  const isPrime = () => {
-    for (let divisor = 2; divisor < question; divisor += 1) {
-      if (question % divisor === 0) {
-        return 'no';
-      }
-    } return 'yes';
-  };
-
-  const correctAnswer = isPrime();
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [correctAnswer, question];
 };
 
