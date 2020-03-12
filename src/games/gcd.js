bbb8ab4ffa3002be73bdc15faa33d868068e8d0d
Alexey Bystrov <alexeybystrov@me.com>
2020-03-12 22:09:28 +0300

    refactor gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,6 +3,15 @@ import randomizer from '../utils.js';
 
 const task = 'Find the greatest common divisor of given numbers.';
 
+const greatestCommonDivisorCalculation = (firstNumber, secondNumber) => {
+  const minNumber = Math.min(firstNumber, secondNumber);
+  const maxNumber = Math.max(firstNumber, secondNumber);
+  let result = minNumber;
+  while (!(minNumber % result === 0 && maxNumber % result === 0)) {
+    result -= 1;
+  } return result;
+};
+
 const generateQuestionAndAnswer = () => {
   const min = 1;
   const max = 30;
@@ -10,16 +19,8 @@ const generateQuestionAndAnswer = () => {
   const randomNumber1 = randomizer(min, max);
   const randomNumber2 = randomizer(min, max);
 
-  const greatestCommonDivisor = (firstNumber, secondNumber) => {
-    const minNumber = Math.min(firstNumber, secondNumber);
-    const maxNumber = Math.max(firstNumber, secondNumber);
-    let result = minNumber;
-    while (!(minNumber % result === 0 && maxNumber % result === 0)) {
-      result -= 1;
-    } return result;
-  };
 
-  const correctAnswer = greatestCommonDivisor(randomNumber1, randomNumber2);
+  const correctAnswer = greatestCommonDivisorCalculation(randomNumber1, randomNumber2);
   const question = `${randomNumber1} ${randomNumber2}`;
   return [correctAnswer.toString(), question];
 };
